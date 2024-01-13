9abc5264578b55852a1ef58efdcbf3d3a70e6a03
Srinivas Vardhan Dintakurthi <vardhan.performs@gmail.com>
2024-01-12 17:40:37 -0800

    new

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import OpenAI from "openai";
 
-const openai = new OpenAI({apiKey:process.env.OPENAI_KEY});
+const openai = new OpenAI({apiKey:process.env.OPENAI_API_KEY});
 
 export async function POST(req: Request, res: NextResponse) {
     const body = await req.json()
