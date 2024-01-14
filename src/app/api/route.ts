ad95981f9cfad92385a9292562551b89d18da28f
Srinivas Vardhan Dintakurthi <vardhan.performs@gmail.com>
2024-01-13 21:52:45 -0800

    updaaaate

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,11 +1,17 @@
 import { NextResponse } from "next/server";
 import OpenAI from "openai";
 
+interface Message {
+  role: string;
+  content: string;
+}
+
+
 const openai = new OpenAI({apiKey:process.env.OPENAI_API_KEY});
 
 export async function POST(req: Request, res: NextResponse) {
     const body = await req.json();
-    const messages=body.messages.map((message)=>({
+    const messages=body.messages.map((message:Message)=>({
       role:message.role,content:message.content,
     }));
 
