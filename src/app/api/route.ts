2694b5e8977a19c25ec22642230e152b04ffbd87
Srinivas Vardhan Dintakurthi <vardhan.performs@gmail.com>
2024-01-13 21:49:50 -0800

    update

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -4,15 +4,22 @@ import OpenAI from "openai";
 const openai = new OpenAI({apiKey:process.env.OPENAI_API_KEY});
 
 export async function POST(req: Request, res: NextResponse) {
-    const body = await req.json()
+    const body = await req.json();
+    const messages=body.messages.map((message)=>({
+      role:message.role,content:message.content,
+    }));
+
+    const requestData = { messages: messages, }; 
   
-    const completion = await openai.chat.completions.create({
-      model: "gpt-3.5-turbo",
-      messages: body.messages,
-    });
-    console.log(completion.choices[0].message);
-    const theResponse = completion.choices[0].message;
-  
+    try{
+      const completion = await openai.chat.completions.create({
+        model: "gpt-3.5-turbo",
+        ...requestData,
+        max_tokens:100,
+      });
+      console.log(completion.choices[0].message);
+      const theResponse = completion.choices[0].message;
+    
     return NextResponse.json({ output: theResponse }, { status: 200 })
-  
+    }catch (error) { console.error(error); return NextResponse.json({ error: "An error occurred" }, { status: 500 }); } 
 };
